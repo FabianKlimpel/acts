1c1cc24b3899f927648b2d0f48c0a06d34eac9cf
FabianKlimpel <fabian.klimpel@tum.de>
2021-04-06 08:56:59 +0200

    Time recording added

--- Tests/IntegrationTests/PropagationTestBase.hpp
+++ Tests/IntegrationTests/PropagationTestBase.hpp
@@ -337,6 +337,107 @@ BOOST_AUTO_TEST_CASE(covariance_transport_to_curvilinear) {
 }
 */
 
+BOOST_AUTO_TEST_CASE(covariance_transport_to_curvilinear) {
+  
+	TFile tf("IntegrationtestsCov.root", "RECREATE");
+	
+	std::default_random_engine gen(42);
+	std::normal_distribution<double> dx(0., sqrt(50_mm));
+	std::normal_distribution<double> dy(0., sqrt(50_mm));
+	std::normal_distribution<double> dz(0., sqrt(100_mm));
+	std::uniform_real_distribution ds(0., 5_m);
+	std::uniform_real_distribution dphi(-M_PI, M_PI);
+	std::uniform_real_distribution dthe(10_degree, 170_degree);
+	std::uniform_real_distribution dp(50_MeV, 100_GeV);
+	std::uniform_int_distribution<> dq(-1, 1);
+  
+    TGraph2D* start1 = new TGraph2D();
+    TGraph* start2 = new TGraph();
+    TGraph2D* end1 = new TGraph2D();
+    TGraph* end2 = new TGraph();
+    TGraph* end3 = new TGraph();
+    TGraph2D* rend1 = new TGraph2D();
+    TGraph* rend2 = new TGraph();
+    TGraph* rend3 = new TGraph();
+    
+	// Sample
+	for(unsigned int i = 0; i < 100000; i++)
+	{		
+		/// Random parameters
+		double x = dx(gen);
+		double y = dy(gen);
+		double z = dz(gen);
+		double phi = dphi(gen);
+		double theta = dthe(gen);
+		double pT = dp(gen);
+		double s = ds(gen);
+		double q = dq(gen);
+
+		//~ std::vector<float> startParams{x, y, z, phi, theta, pT, s, q};
+		start1->SetPoint(i, phi, theta, pT);
+		start2->SetPoint(i, s, q);
+
+		// define start parameters
+		double px = pT * cos(phi);
+		double py = pT * sin(phi);
+		double pz = pT / tan(theta);
+		double time = 0.;
+		Vector3D pos(x, y, z);
+		Vector3D mom(px, py, pz);
+		Vector3D dir = mom.normalized();
+		
+		std::optional<Covariance> covOpt = std::nullopt;
+		Covariance cov;
+		cov <<
+		10_mm, 0, 0.123, 0, 0.5, 0,
+		0, 10_mm, 0, 0.162, 0, 0,
+		0.123, 0, 0.1, 0, 0, 0,
+		0, 0.162, 0, 0.1, 0, 0,
+		0.5, 0, 0, 0, 1_e / 10_GeV, 0,
+		0, 0, 0, 0, 0, 1_ns;
+		// clang-format on
+		covOpt = cov;
+  
+		if(q == 0)
+		{
+			NeutralCurvilinearTrackParameters startN(covOpt, pos, mom, 0);
+			auto covCurvSN = covariance_curvilinear<CurvilinearParameters>(spropagator, startN, s);
+			auto covCurvRN = covariance_curvilinear<CurvilinearParameters>(rspropagator, startN, s);
+			
+			end1->SetPoint(i, covCurvSN(0,0), covCurvSN(1,1), covCurvSN(2,2));
+			end2->SetPoint(i, covCurvSN(3,3), covCurvSN(4,4));
+			end3->SetPoint(i, covCurvSN(5,5), covCurvSN.determinant());
+			rend1->SetPoint(i, covCurvRN(0,0), covCurvRN(1,1), covCurvRN(2,2));
+			rend2->SetPoint(i, covCurvRN(3,3), covCurvRN(4,4));
+			rend3->SetPoint(i, covCurvRN(5,5), covCurvRN.determinant());
+		} else {
+			CurvilinearParameters startC(covOpt, pos, mom, q, 0);
+			auto covCurvEC = covariance_curvilinear<CurvilinearParameters>(epropagator, startC, s);
+			auto covCurvRC = covariance_curvilinear<CurvilinearParameters>(repropagator, startC, s);
+
+			end1->SetPoint(i, covCurvEC(0,0), covCurvEC(1,1), covCurvEC(2,2));
+			end2->SetPoint(i, covCurvEC(3,3), covCurvEC(4,4));
+			end3->SetPoint(i, covCurvEC(5,5), covCurvEC.determinant());
+			rend1->SetPoint(i, covCurvRC(0,0), covCurvRC(1,1), covCurvRC(2,2));
+			rend2->SetPoint(i, covCurvRC(3,3), covCurvRC(4,4));
+			rend3->SetPoint(i, covCurvRC(5,5), covCurvRC.determinant());
+		}
+	}
+	
+	gDirectory->cd();
+	gDirectory->WriteObject(start1, "StartGraph1");
+	gDirectory->WriteObject(start2, "StartGraph2");
+	gDirectory->WriteObject(end1, "EndGraph1");
+	gDirectory->WriteObject(end2, "EndGraph2");
+	gDirectory->WriteObject(end3, "EndGraph3");
+	gDirectory->WriteObject(rend1, "REndGraph1");
+	gDirectory->WriteObject(rend2, "REndGraph2");
+	gDirectory->WriteObject(rend3, "REndGraph3");
+	
+	tf.Write();
+	tf.Close();
+}
+
 BOOST_AUTO_TEST_CASE(dense_covariance_transport) {
 
 	TFile tf("IntegrationtestsCovDense.root", "RECREATE");
@@ -354,9 +455,11 @@ BOOST_AUTO_TEST_CASE(dense_covariance_transport) {
     TGraph2D* start1 = new TGraph2D();
     TGraph* start2 = new TGraph();
     TGraph2D* end1 = new TGraph2D();
-    TGraph2D* end2 = new TGraph2D();
+    TGraph* end2 = new TGraph();
+    TGraph* end3 = new TGraph();
     TGraph2D* rend1 = new TGraph2D();
-    TGraph2D* rend2 = new TGraph2D();
+    TGraph* rend2 = new TGraph();
+    TGraph* rend3 = new TGraph();
     
     DensePropagatorType dpropagator = setupDensePropagator();
 	RiddersPropagator rdpropagator(dpropagator);
@@ -406,19 +509,23 @@ std::cout << "Running iteration " << i << std::endl;
 			auto covCurvRN = covariance_curvilinear<CurvilinearParameters>(rdpropagator, startN, s);
 			
 			end1->SetPoint(i, covCurvSN(0,0), covCurvSN(1,1), covCurvSN(2,2));
-			end2->SetPoint(i, covCurvSN(3,3), covCurvSN(4,4), covCurvSN.determinant());
+			end2->SetPoint(i, covCurvSN(3,3), covCurvSN(4,4));
+			end3->SetPoint(i, covCurvSN(5,5), covCurvSN.determinant());
 			rend1->SetPoint(i, covCurvRN(0,0), covCurvRN(1,1), covCurvRN(2,2));
-			rend2->SetPoint(i, covCurvRN(3,3), covCurvRN(4,4), covCurvRN.determinant());
+			rend2->SetPoint(i, covCurvRN(3,3), covCurvRN(4,4));
+			rend3->SetPoint(i, covCurvRN(5,5), covCurvRN.determinant());
 		} else {
 			CurvilinearParameters startC(covOpt, pos, mom, q, 0);
 			auto covCurvEC = covariance_curvilinear<CurvilinearParameters>(dpropagator, startC, s);
 			auto covCurvRC = covariance_curvilinear<CurvilinearParameters>(rdpropagator, startC, s);
 
 			end1->SetPoint(i, covCurvEC(0,0), covCurvEC(1,1), covCurvEC(2,2));
-			end2->SetPoint(i, covCurvEC(3,3), covCurvEC(4,4), covCurvEC.determinant());
+			end2->SetPoint(i, covCurvEC(3,3), covCurvEC(4,4));
+			end3->SetPoint(i, covCurvEC(5,5), covCurvEC.determinant());
 			rend1->SetPoint(i, covCurvRC(0,0), covCurvRC(1,1), covCurvRC(2,2));
-			rend2->SetPoint(i, covCurvRC(3,3), covCurvRC(4,4), covCurvRC.determinant());
-		}
+			rend2->SetPoint(i, covCurvRC(3,3), covCurvRC(4,4));
+			rend3->SetPoint(i, covCurvRC(5,5), covCurvRC.determinant());
+		}		
 	}
 	
 	gDirectory->cd();
@@ -426,8 +533,10 @@ std::cout << "Running iteration " << i << std::endl;
 	gDirectory->WriteObject(start2, "StartGraph2");
 	gDirectory->WriteObject(end1, "EndGraph1");
 	gDirectory->WriteObject(end2, "EndGraph2");
+	gDirectory->WriteObject(end3, "EndGraph3");
 	gDirectory->WriteObject(rend1, "REndGraph1");
 	gDirectory->WriteObject(rend2, "REndGraph2");
+	gDirectory->WriteObject(rend3, "REndGraph3");
 	
 	tf.Write();
 	tf.Close();
